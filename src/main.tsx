013379a5b4ae29efe88b244b49542aeeb72f94b3
SarahBatagioti <sarah.montuanibt@gmail.com>
2023-05-28 19:25:18 -0300

    configurado com ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
-)
+) 
+
+
